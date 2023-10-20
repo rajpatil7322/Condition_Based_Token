ac6ad4a61592ff9fa1d8b5c6b10175bd9a6a41a1
Raj Patil <rajpatil7322@gmail.com>
2023-10-20 22:43:23 +0530

    eas commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -39,46 +39,46 @@ contract CounterTest is Test {
        assertEq(mytoken.allowance(addr1,address(purposewrapper)),1000);
     }
 
-    function test_transfer_to_purposewrapper() public {
-        test_mint_and_approval();
-        vm.prank(addr1);
-        condition.addCondition(addr3);
-        vm.prank(addr1);
-        purposewrapper.depositFor(addr2,200,address(condition));
-        assertEq(purposewrapper.balanceOf(addr2),200);
-        assertEq(mytoken.balanceOf(addr1),800);
-    }
+    // function test_transfer_to_purposewrapper() public {
+    //     test_mint_and_approval();
+    //     vm.prank(addr1);
+    //     condition.addCondition(addr3);
+    //     vm.prank(addr1);
+    //     purposewrapper.depositFor(addr2,200,address(condition));
+    //     assertEq(purposewrapper.balanceOf(addr2),200);
+    //     assertEq(mytoken.balanceOf(addr1),800);
+    // }
 
-    function test_purposewrapper_condition() public {
-        test_transfer_to_purposewrapper();
-        vm.prank(addr2);
-        purposewrapper.withdrawTo(addr3,100,0);
-        assertEq(purposewrapper.balanceOf(addr2),100);
-        assertEq(mytoken.balanceOf(addr3),100);
-    }
+    // function test_purposewrapper_condition() public {
+    //     test_transfer_to_purposewrapper();
+    //     vm.prank(addr2);
+    //     purposewrapper.withdrawTo(addr3,100,0);
+    //     assertEq(purposewrapper.balanceOf(addr2),100);
+    //     assertEq(mytoken.balanceOf(addr3),100);
+    // }
 
-    function test_purposewrapper_internal_transfer() public {
-        test_transfer_to_purposewrapper();
-        vm.prank(addr2);
-        purposewrapper.transfer(addr4,100,0);
-        assertEq(purposewrapper.balanceOf(addr2),100);
-        assertEq(purposewrapper.balanceOf(addr4),100);
-    }
+    // function test_purposewrapper_internal_transfer() public {
+    //     test_transfer_to_purposewrapper();
+    //     vm.prank(addr2);
+    //     purposewrapper.transfer(addr4,100,0);
+    //     assertEq(purposewrapper.balanceOf(addr2),100);
+    //     assertEq(purposewrapper.balanceOf(addr4),100);
+    // }
 
-    function test_transfer_after_internal_transfer() public{
-        test_purposewrapper_internal_transfer();
-        vm.prank(addr2);
-        purposewrapper.withdrawTo(addr3,100,0);
-        vm.prank(addr4);
-        purposewrapper.withdrawTo(addr3,100,0);
-        assertEq(purposewrapper.balanceOf(addr2),0);
-        assertEq(purposewrapper.balanceOf(addr4),0);
-        assertEq(mytoken.balanceOf(addr3),200);
-    }
+    // function test_transfer_after_internal_transfer() public{
+    //     test_purposewrapper_internal_transfer();
+    //     vm.prank(addr2);
+    //     purposewrapper.withdrawTo(addr3,100,0);
+    //     vm.prank(addr4);
+    //     purposewrapper.withdrawTo(addr3,100,0);
+    //     assertEq(purposewrapper.balanceOf(addr2),0);
+    //     assertEq(purposewrapper.balanceOf(addr4),0);
+    //     assertEq(mytoken.balanceOf(addr3),200);
+    // }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        vm.prank(addr1);
-        mytoken.mint(x);
-        assertEq(mytoken.balanceOf(addr1), x);
-    }
+    // function testFuzz_SetNumber(uint256 x) public {
+    //     vm.prank(addr1);
+    //     mytoken.mint(x);
+    //     assertEq(mytoken.balanceOf(addr1), x);
+    // }
 }
